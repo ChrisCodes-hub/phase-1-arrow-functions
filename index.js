c9855883d17816389fa47803493d20d72455e9c3
christopherchege <chrisshege35@gmail.com>
2025-03-14 15:27:12 +0300

    added javascript file

---
+++ index.js
@@ -0,0 +1,12 @@
+//function add(a, b) {
+  //  return a + b;
+  //}
+//const add = (a, b) => a + b;
+function divide(num1, num2){
+    return num1/num2;
+}
+console.log(divide(2000,100));
+
+let square = x => x*x
+
+const add = (par1,par2)=> par1+par2
